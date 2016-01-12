0f2358838a24d4483554fd9782c90ebe9ce72824
Michael DiGuiseppi <michael.diguiseppi@gmail.com>
2016-01-12 16:31:26 -0700

    finalized slack problem code

--- slack-args-test.js
+++ slack-args-test.js
@@ -1,21 +1,19 @@
 function myFn (first, second, third) {
     if (arguments.length !== 0) {
-        if (arguments.length > 3) {
-            console.log("AARG! Too many arguments!");
-            console.log(arguments);
-            }
-        else {
             console.log("Beginning to read arguments...")
             for (var index in arguments) {
-                if (arguments[index] !== undefined) {
+                if (parseInt(index) > 2) {
+                    console.log("AARG! Too many arguments!");
+                    console.log(arguments);
+                }
+                else if (arguments[index] !== undefined) {
                     console.log("Reading argument #" + (parseInt(index) + 1) + ": " + arguments[index]);
                 }
             }
         }
-    }
     else {
         console.log("No arguments!");
     }
 };
 
-myFn(undefined, "second", undefined, "fourth");
+myFn();
